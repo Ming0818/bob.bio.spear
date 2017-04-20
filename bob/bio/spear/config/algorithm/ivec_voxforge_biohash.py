4242b3197ace4c8270958ba59a2244de7f116093
Vedrana KRIVOKUCA <vkrivokuca@italix32.idiap.ch>
2017-04-20 14:37:18 +0200

    Add comments to ivec_voxforge_biohash and ivec_voxforge_biohash_ST configurations

--- bob/bio/spear/config/algorithm/ivec_voxforge_biohash.py
+++ bob/bio/spear/config/algorithm/ivec_voxforge_biohash.py
@@ -1,3 +1,12 @@
+""" 
+Protects IVectors using BioHash in the NORMAL scenario (each user has a unique secret seed, which is used to initialise 
+the projection matrix from which the user's BioHash is generated)
+
+Note: The "length" parameter should be changed according to how many bits you wish your BioHash to consist of.  The 
+maximum BioHash length should not exceed the value of the "subspace_dimension_of_t" parameter, since the BioHash cannot
+have more dimensions than the original IVector.
+"""
+
 from bob.bio.gmm.algorithm.IVector_BioHash import IVector_BioHash
 
 algorithm = IVector_BioHash(subspace_dimension_of_t = 50, use_lda = False, use_wccn = False, use_plda = False, length = 50)
--- bob/bio/spear/config/algorithm/ivec_voxforge_biohash_ST.py
+++ bob/bio/spear/config/algorithm/ivec_voxforge_biohash_ST.py
@@ -1,3 +1,17 @@
+""" 
+Protects IVectors using BioHash in the STOLEN TOKEN scenario (impostor steals genuine user's secret seed, so projection 
+matrix used to generate the impostor's BioHash is the same as that used to generate the genuine user's BioHash)
+
+Notes: 
+
+1. The "length" parameter should be changed according to how many bits you wish your BioHash to consist of.  The 
+maximum BioHash length should not exceed the value of the "subspace_dimension_of_t" parameter, since the BioHash cannot
+have more dimensions than the original IVector.
+
+2. The "user_seed" parameter can be set to any integer.  In this configuration file, user_seed = length for simplicity
+and ease of debugging.
+"""
+
 from bob.bio.gmm.algorithm.IVector_BioHash import IVector_BioHash
 
 algorithm = IVector_BioHash(subspace_dimension_of_t = 400, use_lda = False, use_wccn = False, use_plda = False, length = 400, user_seed = 400)
