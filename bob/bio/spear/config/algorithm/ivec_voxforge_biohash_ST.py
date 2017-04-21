d90c91f0ff2cf785a5728c6c710451edeac99d8e
Vedrana KRIVOKUCA <vkrivokuca@italix32.idiap.ch>
2017-04-21 10:03:32 +0200

    Set BioHash length to 10 in ST scenario for IVectors, as an example of how to run the code

--- bob/bio/spear/config/algorithm/ivec_voxforge_biohash_ST.py
+++ bob/bio/spear/config/algorithm/ivec_voxforge_biohash_ST.py
@@ -14,4 +14,4 @@ and ease of debugging.
 
 from bob.bio.gmm.algorithm.IVector_BioHash import IVector_BioHash
 
-algorithm = IVector_BioHash(subspace_dimension_of_t = 400, use_lda = False, use_wccn = False, use_plda = False, length = 400, user_seed = 400)
+algorithm = IVector_BioHash(subspace_dimension_of_t = 400, use_lda = False, use_wccn = False, use_plda = False, length = 10, user_seed = 10)
